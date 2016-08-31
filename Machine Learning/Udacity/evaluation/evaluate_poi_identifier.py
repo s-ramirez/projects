407b9040cdb36fe98f56c63c83b5a30e32147a0b
Sebastian Ramirez <sramirez10r@gmail.com>
2016-08-31 12:12:41 -0400

    Finishing ML Course

--- Machine Learning/Udacity/evaluation/evaluate_poi_identifier.py
+++ Machine Learning/Udacity/evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,9 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import tree
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.cross_validation import train_test_split
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -26,6 +29,14 @@ labels, features = targetFeatureSplit(data)
 
 
 
-### your code goes here 
+### your code goes here
 
-
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels,test_size = 0.3, random_state = 42)
+#print(labels_test)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+predicted = clf.predict(features_test)
+acc = accuracy_score(predicted, labels_test)
+#print(acc)
+#print(precision_score(labels_test, predicted))
+print(recall_score(labels_test, predicted))
